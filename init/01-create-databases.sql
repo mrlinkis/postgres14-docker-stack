88e2673dc37b86c7973725aa8c17ac584b3609b7
mrlinkis <226179592+mrlinkis@users.noreply.github.com>
2025-10-30 05:55:32 +0300

    Create 01-create-databases.sql

---
+++ init/01-create-databases.sql
@@ -0,0 +1,6 @@
+-- Создаём базы
+CREATE DATABASE "ESOG.Auth";
+CREATE DATABASE "ESOG.Fias";
+CREATE DATABASE "IPCP";
+CREATE DATABASE "HelpService";
+CREATE DATABASE "SSTUxxx";  -- Заменяй xxx на номер площадки перед развёртыванием
